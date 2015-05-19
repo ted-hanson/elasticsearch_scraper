38f8003232c8202926bf942fb7847b235b1d8c00
Ted Hanson <ted.hanson@moovweb.com>
2015-05-19 16:33:46 -0700

    update es_to_csv.rb to get ALL logs (if you don't specify size)

--- es_to_csv.rb
+++ es_to_csv.rb
@@ -5,29 +5,36 @@ require 'json'
 data = ARGV[0]
 size = ARGV[1] if !ARGV[1].nil?
 
-size = size.to_i
+req = `curl 'http://logs.moov.sh:9200/logstash-*/_search?from=0&size=1' -d '#{data}' -s`
 
-i = 10200000
-while i < size do
-  puts "downloading index #{i}"  
-  amt = [size-i, 1].min 
-  puts amt
-  x = `curl -POST 'http://logs.moov.sh:9200/logstash-2015.01.23/_search?from=#{i}&size=#{amt}' -d '#{data}'`
 
-  CSV.open("logs-#{Time.new.strftime("%m.%d.%y")}.csv", "ab") do |csv|
-    #get all hits
+CSV.open("logs-#{Time.new.strftime("%m.%d.%y")}.csv", "ab") do |csv|
+  #get all hits
+  j = JSON.parse(req)
+  size = j['hits']['total']
+  j = j['hits']['hits']
+
+  #push keys once
+  csv << j[0]['_source'].select{|k,v| k != 'message'}.keys
+
+  # pull 10000 values at once
+  amt = 10000
+  i = 0
+  while i < size do
+    puts "downloading index #{i}"
+    puts amt
+    x = `curl -POST 'http://logs.moov.sh:9200/logstash-*/_search?from=#{i}&size=#{amt}' -d '#{data}' -s`
+
     j = JSON.parse(x)['hits']['hits']
-    #push keys once
-    csv << j[0]['_source'].select{|k,v| k != 'message'}.keys
     #iterate through all values and push
     j.each do |hash|
       begin
-        csv << hash['_source'].select{|k,v| k != 'message'}.values 
+        csv << hash['_source'].select{|k,v| k != 'message'}.values
       rescue
         puts "Error: #{hash}"
       end
     end
-  end
 
-  i += 100000
+    i += amt
+  end
 end
